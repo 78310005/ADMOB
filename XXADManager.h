dc9fbb83367c5f6cfab368f49d375ec1fabe18bc
Dafa <78310005@qq.com>
2019-09-29 09:35:42 +0800

    Add files via upload

---
+++ XXADManager.h
@@ -0,0 +1,17 @@
+//
+//  XXADManager.h
+//  XXAD
+//
+//  Created by SWKJ on 2019/9/24.
+//  Copyright © 2019 XX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXADManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
